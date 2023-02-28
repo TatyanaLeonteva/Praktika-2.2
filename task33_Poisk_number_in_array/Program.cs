0fb4d409745f1b110ac43686611b26cc74a2ab2f
Tatyana Leonteva <liwanda@yandex.ru>
2023-02-28 19:34:23 +0300

    seminar5

---
+++ task33_Poisk_number_in_array/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 3, 19, 345, 3] -> да
+
+int[] FillArrayWithRandomNumbers(int length)
+{
+    int[] arr = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-19, 20);
+    }
+    return arr;
+}
+
+bool isOk(int[] array, int number)
+{
+    //bool trust = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (number == array[i])
+        {
+            //bool trust = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+System.Console.WriteLine("Введите число, которое нужно найти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (isOk(array, number))
+{
+    System.Console.WriteLine("Элемент найден");
+}
+else
+{
+    System.Console.WriteLine("Элемент не найден");
+}
---
+++ task35_N_v_otrezke10-99/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 35: Задайте одномерный массив из N случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] FillArrayWithRandomNumbers(int length)
+{
+    int[] arr = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 150);
+    }
+    return arr;
+}
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+
+if (count == 1)
+{
+    System.Console.WriteLine($"{(count)} элемент лежит в отрезке [10,99]");
+}
+else 
+    if (count < 5)
+    {
+       System.Console.WriteLine($"{(count)} элемента лежат в отрезке [10,99]");
+    }
+else
+System.Console.WriteLine($"{(count)} элементов лежат в отрезке [10,99]");
