f271a3eb7a45c3e587205f46c0378aab9ef6dd3d
Tatyana Leonteva <liwanda@yandex.ru>
2023-03-05 23:01:10 +0300

    Seminar2

--- task12_Kratno/Program.cs
+++ task12_Kratno/Program.cs
@@ -5,10 +5,10 @@
 //34, 5 -> не кратно, остаток 4 
 //16, 4 -> кратно
 
-Console.Write("Введите Number5: ");
+Console.Write("Введите Number1: ");
 int Number5 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите Number6: ");
+Console.Write("Введите Number2: ");
 int Number6 = Convert.ToInt32(Console.ReadLine());
 
 int Div = Number6/Number5;
@@ -19,5 +19,20 @@ if(Div%10==0)
 }
 else
 {
-Console.WriteLine($"Остаток от деления: {Div%10}");
+Console.WriteLine($"Не кратно, остаток от деления {Div%10}");
 }
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int mod = number1 % number2;
+// if(mod == 0)
+// {
+// Console.Write("Кратно");
+// }
+// else
+// {
+// Console.Write($"Не кратно, остаток {mod}");
+// }
---
+++ task16_N1_square_N2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №16. Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите число 1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 * N1 == N2 || N2 * N2 == N1)
+{
+    if (N1 > N2)
+    {
+        Console.WriteLine($"{N2} в квадрате = {N1}");
+    }
+    else
+    {
+        Console.WriteLine($"{N1} в квадрате = {N2}");
+    }
+}
+else
+{
+    Console.WriteLine($"Нет");
+}
