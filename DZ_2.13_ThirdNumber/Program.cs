c3c058db4991c5058bfe66372df85509b76f9ec8
Tatyana Leonteva <liwanda@yandex.ru>
2023-03-05 16:26:59 +0300

    DZ_2

---
+++ DZ_2.13_ThirdNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > -100 && N < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if(Math.Abs(N)>999)
+{
+    while (Math.Abs(N) > 999)
+    {
+        N = N / 10;
+    }
+    int thirdNumber = Math.Abs(N) % 10;
+    Console.WriteLine($"{thirdNumber}");
+}
+else
+{
+    int thirdNumber = Math.Abs(N) % 10;
+    Console.WriteLine($"{thirdNumber}");
+}
---
+++ DZ_2.15_Weekends/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if( day==6 || day==7)
+{
+    Console.WriteLine($"Выходной");
+}
+else if(day>=6 || day<=0)
+{
+    Console.WriteLine($"Не существует");
+}
+else 
+{
+    Console.WriteLine($"Рабочий");
+}
