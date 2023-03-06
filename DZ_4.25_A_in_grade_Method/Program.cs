4155fc2de10968eb05c1aae65850937c033a6ec4
Tatyana Leonteva <liwanda@yandex.ru>
2023-03-06 22:42:10 +0300

    DZ_4

--- DZ_4.25_A_in_grade_Method/Program.cs
+++ DZ_4.25_A_in_grade_Method/Program.cs
@@ -4,29 +4,36 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число: ");
-int A = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите натуральную степень, в которую нужно возмести число: ");
-int B = Convert.ToInt32(Console.ReadLine());
-
-int temp = A;
-
-if (B <= 0)
+int[] AiB()
 {
-    Console.Write($"\nНатуральная степень не может быть отрицательной или равной 0");
-    System.Console.WriteLine("\n");
-    return;
+    Console.Write("Введите число А: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите натуральную степень, в которую нужно возмести число: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    int[] array={A,B};
+    return array;
 }
-else
+
+int InGrade(int[] array1)
 {
-    for (int i = 2; i <= B; i++)
+    int temp=array1[0];
+    for(int i=1; i<array1[1]; i++)
     {
-        temp = temp * A;
+        temp = temp * array1[0];
     }
-    System.Console.Write($"\n{A} в степени {B} = {temp}");
+    return temp;
 }
-System.Console.WriteLine("\n");
+
+System.Console.WriteLine(InGrade(AiB()));
+
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите натуральную степень, в которую нужно возмести число: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int temp = A;
 
 // if (B <= 0)
 // {
@@ -34,4 +41,13 @@ System.Console.WriteLine("\n");
 //     System.Console.WriteLine("\n");
 //     return;
 // }
-// System.Console.Write($"{Math.Pow(A, B)}");
+// else
+// {
+//     for (int i = 2; i <= B; i++)
+//     {
+//         temp = temp * A;
+//     }
+//     System.Console.Write($"\n{A} в степени {B} = {temp}");
+// }
+// System.Console.WriteLine("\n");
+
--- DZ_4.29_Array_8El/Program.cs
+++ DZ_4.29_Array_8El/Program.cs
@@ -3,9 +3,11 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] Array8Elements(int length)
+int[] Array8Elements(int Name) 
+// int - тип возвращаемого значения, [] - одномерный массив, [,] многомерный массив
+// в круглых скобках - аргумент, то, что принимает: int - тип аргумента, Name - любое название
 {
-    int[] array = new int[length];
+    int[] array = new int[Name];
     
     Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
@@ -15,6 +17,6 @@ int[] Array8Elements(int length)
     return array;
 }
 
-int length = 8;
-int[] array8El = Array8Elements(length);
+int Name = 15;
+int[] array8El = Array8Elements(Name);
 System.Console.WriteLine($"\nМассив из 8 элементов [{string.Join(", ", array8El)}]\n");
